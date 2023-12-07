093f9c8a8d1a96c6a0819ba74845d388c5962484
shubhamdogra418 <shubhamdogra418@gmail.com>
2023-12-07 14:26:51 +0530

    pro

--- Array/binarySearch.java
+++ Array/binarySearch.java
@@ -1,6 +1,6 @@
 class binarySearch {
     public static void main(String[] args) {
         System.out.println("binary search");
-        System.out.println("nothing to add why");
+        System.out.println("have to wrie the program");
     }
 }
