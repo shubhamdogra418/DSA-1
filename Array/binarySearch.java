af2b61296030f17dfaa26d9be1c0c50e1cf18c7c
shubhamdogra418 <shubhamdogra418@gmail.com>
2023-12-08 01:00:04 +0530

    BS code

--- Array/binarySearch.java
+++ Array/binarySearch.java
@@ -1,6 +1,23 @@
 class binarySearch {
+    public static int binarySearch (int arr[],int target) {
+        int low=0;
+        int high=arr.length;
+        while (low <= high) {
+            int mid= low+ (high-low)/2;
+            if(target > arr[mid]) {
+                low=mid+1;
+            } else if (target < arr[mid]) {
+                high=mid-1;
+            } else {
+                return mid;
+            }
+        } return -1;
+    }
     public static void main(String[] args) {
-        System.out.println("binary search");
-        System.out.println("have to wrie the program");
+        int arr[]={2,4,6,8,19,25};
+        int target=19;
+        System.out.println("Target found at index no.: "+binarySearch(arr, target));
+
+
     }
 }
