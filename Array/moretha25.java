0e7549dd5048d31a3cc00baa31e099f6eb87b3a1
shubhamdogra418 <shubhamdogra418@gmail.com>
2023-12-12 02:39:00 +0530

    LC-1287 easy

---
+++ Array/moretha25.java
@@ -0,0 +1,33 @@
+class moretha25 {
+    public static int morethan (int arr[]) {
+        int n=arr.length;
+        for(int i=0;i<arr.length;i++) {
+            int count=0;
+            for(int j=i;j<n;j++) {
+                if(arr[i]==arr[j]) {
+                    count++;
+                } 
+            } if(count > n/4) {
+                return arr[i];
+            }
+        } return -1;
+    }
+
+    public static int anotherOp (int arr[]) {
+        int n=arr.length;
+        int count=1;
+        for(int i=0;i<n-1;i++) {
+            if(arr[i]==arr[i+1]) {
+                count++;
+             if (count > n/4) return arr[i]; 
+            } else {
+                count=1; 
+            } 
+        } return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,2,6,6,6,6,7,10};
+        System.out.println(morethan(arr));
+        System.out.println(anotherOp(arr));
+    }
+}
